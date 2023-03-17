ab6ce178f1b03a43d5943cd3d74606044e864bb4
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-03-17 20:08:59 +0530

    modal base

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Profile from './pages/Profile';
+import Simple from './components/modals/Base';
 
 function App() {
   return (
     <div className=''>
 <Navbar/>
 <Profile/>
+<Simple children={<></>} isOpen={false}/>
 <Footer/>
     </div>
   );
---
+++ src/components/modals/Base.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Modal from 'react-modal'
+import './modal.css'
+function Simple({children,isOpen}:{children:any;isOpen:boolean}) {
+  return (
+  <Modal  contentLabel="Modal"
+  className={{
+    base: 'modal-base',
+    afterOpen: 'modal-base_after-open',
+    beforeClose: 'modal-base_before-close'
+  }}
+  overlayClassName={{
+    base: 'overlay-base',
+    afterOpen: 'overlay-base_after-open',
+    beforeClose: 'overlay-base_before-close'
+  }}
+  shouldCloseOnOverlayClick={true}
+  closeTimeoutMS={2000}
+   isOpen={isOpen}>
+    <div className='bg-[#F8F9FC] p-4 rounded-2xl'>
+        {children}
+    </div>
+    </Modal>
+  )
+}
+
+export default Simple
